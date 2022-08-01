cae2430f50894222047d8eb79c97ab441854b5ea
MegaNep <nep@nepmia.fr>
2022-08-01 23:41:11 +0200

    Added logging basic config with dotenv

--- converter.py
+++ converter.py
@@ -1,6 +1,24 @@
 import sys
 import os
+import logging as log
 from PIL import Image
+
+from helpers import verify_logs, create_log_folder, log_base_dir
+
+if not verify_logs():
+    create_log_folder()
+
+log.basicConfig(
+    encoding="utf-8",
+    level=log.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        log.FileHandler(f"{log_base_dir}/converter.log"),
+        log.StreamHandler(sys.stdout),
+    ],
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
 
 def convert_images(path_to_exec:str, extension_to_convert:str):
     
