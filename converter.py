2bc49542f018d7f83b8d3c5ec4cfdc5e845b7bf0
MegaNep <nep@nepmia.fr>
2022-08-02 02:56:02 +0200

    Added support for ignores, you can now ignore some files

--- converter.py
+++ converter.py
@@ -23,7 +23,7 @@ log.basicConfig(
 )
 
 
-def convert_images(path_to_exec:str, source_extension:str or list, target_extension:str or list, case:str, overwrite:bool):
+def convert_images(path_to_exec:str, source_extension:list, target_extension:list, ignore_names: list, case:str, overwrite:bool):
     
     # Define empty file list
     file_list = []
@@ -45,44 +45,53 @@ def convert_images(path_to_exec:str, source_extension:str or list, target_extens
     
     log.info("Folder listed, converting images.")
     for file in file_list:
+
+
+
         log.info(f"Converting {file}")
         for extension in source_extension:
+
             raw_file_name = file.replace(f'.{extension}', '')
+
+            # Check if file should be ignored
+            if raw_file_name in ignore_names:
+                log.warning(f"File {file} is in ignore list, skipping.")
             
-            # Create the correct name for the exported file
-            parsed_file_name = eval(f"humps.{allowed_cases[case]}('{raw_file_name}')")
-            
-
-            if file.endswith(extension):
+            else:
+                # Create the correct name for the exported file
+                parsed_file_name = eval(f"humps.{allowed_cases[case]}('{raw_file_name}')")
                 
-                for new_extension in target_extension:
-                
-                    # Defaults to true and will change with prompt
-                    answer = True
-                
-                    # If the file already exist and if no overwrite rule has
-                    # Been set, ask the user if they want to overwrite
-                    if f"{parsed_file_name}.{new_extension}" in file_list and not overwrite:
-                        answer = user_prompt(
-                            basic_answers, 
-                            f"File {parsed_file_name}.{new_extension} already exist, overwrite?", 
-                            basic_yes_default,
-                            "yes"
-                        )
 
-                    if answer:
+                if file.endswith(extension):
+                    
+                    for new_extension in target_extension:
+                    
+                        # Defaults to true and will change with prompt
+                        answer = True
+                    
+                        # If the file already exist and if no overwrite rule has
+                        # Been set, ask the user if they want to overwrite
+                        if f"{parsed_file_name}.{new_extension}" in file_list and not overwrite:
+                            answer = user_prompt(
+                                basic_answers, 
+                                f"File {parsed_file_name}.{new_extension} already exist, overwrite?", 
+                                basic_yes_default,
+                                "yes"
+                            )
 
-                        log.info(f"Converting {file} to {parsed_file_name}.{new_extension}")
-                        image = Image.open(os.path.join(path_to_exec, file))
+                        if answer:
 
-                        # Special case of jpeg...
-                        if new_extension == "jpg" or "jpeg":
-                            log.warning("User is converting in jpeg, converting RGBA to RGB.")
-                            image = image.convert("RGB")
+                            log.info(f"Converting {file} to {parsed_file_name}.{new_extension}")
+                            image = Image.open(os.path.join(path_to_exec, file))
 
-                        image.save(f"{path_to_exec}/{parsed_file_name}.{new_extension}")
-                    else:
-                        log.warning(f"User denied overwriting of file {parsed_file_name}.{new_extension}, skipping it.")
+                            # Special case of jpeg...
+                            if new_extension == "jpg" or "jpeg":
+                                log.warning("User is converting in jpeg, converting RGBA to RGB.")
+                                image = image.convert("RGB")
+
+                            image.save(f"{path_to_exec}/{parsed_file_name}.{new_extension}")
+                        else:
+                            log.warning(f"User denied overwriting of file {parsed_file_name}.{new_extension}, skipping it.")
 
                 
     log.info("Job finished.")
@@ -92,8 +101,9 @@ if __name__ == "__main__":
     parser.add_argument("path_to_exec", help="Path to the folder that contains all the images we want to convert")
     parser.add_argument("-s", "--source-extensions", nargs="*", help="List of extensions that needs to be converted (can be single)")
     parser.add_argument("-t", "--target-extensions", nargs="*", help="List of extensions for the newly created files (can be single)")
-    parser.add_argument("-o","--overwrite", required=False, default=False, help="Option to always overwrite files", action="store_true")
+    parser.add_argument("-i", "--ignore-names", nargs="*", required=False, default=[], help="List of file names that should be ignored (can be single and must be exact without extension)")
     parser.add_argument("-c", "--case", required=False, default="snake", help="Option to select export casing, default is snake case. Supports: camel, snake, kebab, pascal")
+    parser.add_argument("-o","--overwrite", required=False, default=False, help="Option to always overwrite files", action="store_true")
     
     # Parse the args
     args = parser.parse_args()
@@ -104,7 +114,13 @@ if __name__ == "__main__":
     if args.overwrite:
         log.warn("Overwriting automatically.")
 
-    log.info(args)
     # Launch the conversion
-    convert_images(args.path_to_exec, args.source_extensions, args.target_extensions, args.case, args.overwrite)
+    convert_images(
+        args.path_to_exec, 
+        args.source_extensions, 
+        args.target_extensions, 
+        args.ignore_names,
+        args.case, 
+        args.overwrite
+    )
 
